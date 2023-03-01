e5ff1bd22b47240c45f131486a32af28fd972c16
JaeggerJose <lms025187@gmail.com>
2023-03-01 16:39:39 +0800

    practice01

---
+++ button.js
@@ -0,0 +1,7 @@
+function changeResult() {
+    var myTable = document.getElementById("csie");
+    myTable.rows[1].cells[2].innerHTML="吳齊人";
+    myTable.rows[2].cells[2].innerHTML="趙一平";
+    myTable.rows[3].cells[2].innerHTML="李季青";
+}
+function displayResult(){document.getElementById("Hellow world!").innerHTML = "Have a nice day!";}
